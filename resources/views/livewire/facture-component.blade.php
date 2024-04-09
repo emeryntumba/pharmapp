08ea5db9a07d040f8cfb7535b209860b9f659189
Emery <emeryntumba39@outlook.com>
2024-04-09 16:30:37 +0100

    fix: proforma invoice devise

--- resources/views/livewire/facture-component.blade.php
+++ resources/views/livewire/facture-component.blade.php
@@ -30,7 +30,7 @@
                             <td class="border-bottom-0"><p class="mb-0 fs-2">{{ strlen($product['produit']->nom) > 5 ? substr($product['produit']->nom, 0, 7) . '...' : $product['produit']->nom }}</p></td>
                             <td class="border-bottom-0"><h6 class=" mb-0 fs-2">{{ $product['produit']->prix }} {{ session('devise') }}</h6></td>
                             <td class="border-bottom-0"><input type="text" class="form-control px-2 fs-2" wire:model="products.{{ $index }}.quantite" wire:input="recalculerTotal({{ $index }})"></td>
-                            <td class="border-bottom-0"><span class=" mb-0 fs-2" >{{ $product['total'] }}FC</span></td>
+                            <td class="border-bottom-0"><span class=" mb-0 fs-2" >{{ $product['total'] }} {{ session('devise') }}</span></td>
                             <td class="border-bottom-0"><button class="btn btn-danger fs-2 p-2" wire:click="delete({{$index}})"  wire:confirm="Etes-vous sur de vouloir supprimer ce produit?"><i class="ti ti-trash"></i></button></td>
                         </tr>
 
