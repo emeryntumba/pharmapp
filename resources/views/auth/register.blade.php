30e6a4c0663ff6f5f8fbfc268492f8075f9af345
Emery <emeryntumba39@outlook.com>
2024-04-23 20:43:16 +0100

    chore: set index route for auth files to their logo images

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
           <div class="col-md-8 col-lg-6 col-xxl-3">
             <div class="card mb-0">
               <div class="card-body">
-                <a href="./index.html" class="text-nowrap logo-img text-center d-block py-3 w-100">
+                <a href="{{ route('index') }}" class="text-nowrap logo-img text-center d-block py-3 w-100">
                   <img src="../assets/images/logos/logo.png" width="180" alt="">
                 </a>
                 <p class="text-center">Gérer votre pharmacie devient facile</p>
