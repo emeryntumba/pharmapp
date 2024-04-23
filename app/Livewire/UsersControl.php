e4d856e30efd14bee0a60076f827a7ead0beddbb
Emery <emeryntumba39@outlook.com>
2024-04-23 20:34:39 +0100

    feat: display user table in setting of app

--- app/Livewire/UsersControl.php
+++ app/Livewire/UsersControl.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Gestionnaire;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UsersControl extends Component
 {
     public function render()
     {
-        return view('livewire.users-control');
+        $etablissement = Auth::user()->gestionnaire->etablissement->id;
+        $gestionnaires = Gestionnaire::where('etablissement_id', $etablissement)->get();
+        $users = $gestionnaires->pluck('user');
+        
+        return view('livewire.users-control', [
+            'users' => $users,
+        ]);
     }
 }
--- resources/views/livewire/users-control.blade.php
+++ resources/views/livewire/users-control.blade.php
@@ -1,31 +1,41 @@
-<div class="table-responsive">
-    <table class="table table-hover text-nowrap mb-0 align-middle ">
-        <thead class="text-dark fs-4 ">
-            <tr>
+<div class="row">
+    <div class="col-12 table-responsive">
+        <table class="table table-hover text-nowrap mb-0 align-middle ">
+            <thead class="text-dark fs-4 ">
+                <tr>
 
-                <th class="border-bottom-0" wire:click="sortBy('nom')">
-                    <h6 class="fw-semibold mb-0">Nom</h6>
-                </th>
-                <th class="border-bottom-0" wire:click="sortBy('prix')">
-                    <h6 class="fw-semibold mb-0">Email</h6>
-                </th>
-                <th class="border-bottom-0" >
-                    <h6 class="fw-semibold mb-0">Mot de passe</h6>
-                </th>
-                <th class="border-bottom-0" wire:click="sortBy('dosage')">
-                    <h6 class="fw-semibold mb-0">Role</h6>
-                </th>
-                <th class="border-bottom-0" wire:click="sortBy('forme_galenique')">
-                    <h6 class="fw-semibold mb-0">Date enregistrement</h6>
-                </th>
-                <th class="border-bottom-0" wire:click="sortBy('forme_galenique')">
-                    <h6 class="fw-semibold mb-0">Actions</h6>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr></tr>
-        </tbody>
-    </table>
+                    <th class="border-bottom-0" wire:click="sortBy('nom')">
+                        <h6 class="fw-semibold mb-0">Nom</h6>
+                    </th>
+                    <th class="border-bottom-0" wire:click="sortBy('prix')">
+                        <h6 class="fw-semibold mb-0">Email</h6>
+                    </th>
+
+                    <th class="border-bottom-0" wire:click="sortBy('dosage')">
+                        <h6 class="fw-semibold mb-0">Role</h6>
+                    </th>
+
+                    <th class="border-bottom-0" wire:click="sortBy('forme_galenique')">
+                        <h6 class="fw-semibold mb-0">Actions</h6>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td class="border-bottom-0">{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->getRoleNames()->first() }}</td>
+                        <td class="border-bottom-0">
+                            <a href=""><button class="btn btn-primary"><i class="ti ti-eye"></i></button></a>
+                            <a href=""><button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#Modal2"><i class="ti ti-pencil"></i></button></a>
+                            <button class="btn btn-danger"  wire:confirm="Etes-vous sur de vouloir supprimer ce produit?"><i class="ti ti-trash"></i></button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 
 </div>
