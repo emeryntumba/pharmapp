736d0a862f1b89e17afb9e055f874d7b875ebaa8
Emery <emeryntumba39@outlook.com>
2024-05-13 08:32:55 +0100

    chore: change menu link

--- resources/views/landing_page/components/header.blade.php
+++ resources/views/landing_page/components/header.blade.php
@@ -9,9 +9,9 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Accueil</a></li>
-          <li><a class="nav-link scrollto" href="#about">Fonctionnalités</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Prix</a></li>
-          <li><a class="nav-link scrollto" href="#services">FAQ</a></li>
+          <li><a class="nav-link scrollto" href="#fonctionnalites">Fonctionnalités</a></li>
+          <li><a class="nav-link scrollto " href="#princing">Prix</a></li>
+          <li><a class="nav-link scrollto" href="#faq">FAQ</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
           <li><a class="getstarted scrollto" href="{{ route('etablissement.create') }}">Commencer</a></li>
         </ul>
