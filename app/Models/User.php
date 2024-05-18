795e4fe3a61f30513a042feec603466842cf955a
Emery <emeryntumba39@outlook.com>
2024-05-18 11:51:31 +0100

    feat: add payment method

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use LemonSqueezy\Laravel\Billable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -14,6 +15,7 @@ class User extends Authenticatable
     use HasApiTokens;
     use HasFactory;
     use HasRoles;
+    use Billable;
 
     use Notifiable;
 
